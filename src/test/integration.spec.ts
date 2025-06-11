1996c7a269c8db17b984a7304c4e01f557452c40
Simon Wulf <siwu.lund@gmail.com>
2025-06-11 23:55:02 +0200

    Remove unused integration.spec.ts

--- src/test/integration.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('integration: cart-parser + cart-writer', function() {
-  it('should ...', function() {
-    // TODO: add integration test for parser and writer working together
-  });
-});
